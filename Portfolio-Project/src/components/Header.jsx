10a0980df1b8bb00f2352e01e46429057caa5f4a
Ignacio Agustín Díaz Neila <92888908+IgnacioDN@users.noreply.github.com>
2026-09-21 20:25:04 -0300

    Change 'Projects' button to 'Education' in Header

--- Portfolio-Project/src/components/Header.jsx
+++ Portfolio-Project/src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
             <li><button onClick={() => scrollToSection('about')}>About</button></li>
             <li><button onClick={() => scrollToSection('experience')}>Experience</button></li>
             <li><button onClick={() => scrollToSection('skills')}>Skills</button></li>
-            <li><button onClick={() => scrollToSection('projects')}>Projects</button></li>
+            <li><button onClick={() => scrollToSection('education')}>Projects</button></li>
             <li><button onClick={() => scrollToSection('contact')}>Contact</button></li>
           </ul>
         </nav>
